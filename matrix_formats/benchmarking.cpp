e1bce65f69ca96f61cdb54d3bda1529900634b0d
susdivita <150903731+susdivita@users.noreply.github.com>
2023-11-16 00:56:17 +0000

    Stride-1 pattern for mxv

--- matrix_formats/benchmarking.cpp
+++ matrix_formats/benchmarking.cpp
@@ -5,16 +5,17 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <chrono>
+#include <omp.h>
+
 struct Triplet {
-  int  row, col;
+  int row, col;
   double value;
   Triplet(int r, int c, double v) : row(r), col(c), value(v) {}
 };
 using Triplets = std::vector<Triplet>;
 using Vector = std::vector<double>;
 using index_t = std::ptrdiff_t;
-
-
 
 class EllpackMat {
  public:
@@ -28,16 +29,7 @@ class EllpackMat {
   index_t maxcols;  //< Number of non-empty columns
   index_t m, n;     //< Number of rows, number of columns
 };
-/*
-double EllpackMat::operator()(index_t i, index_t j) const {
-  assert(0 <= i && i < m && 0 <= j && j < n && "Index out of bounds!");
 
-  for (index_t l = i * maxcols; l < (i + 1) * maxcols; ++l) {
-    if (col.at(l) == j) return val.at(l);
-  }
-  return 0;
-}
-*/
 EllpackMat::EllpackMat(const Triplets &triplets, index_t m, index_t n)
     : m(m), n(n) {
 
@@ -45,9 +37,11 @@ EllpackMat::EllpackMat(const Triplets &triplets, index_t m, index_t n)
   std::fill(counters.begin(), counters.end(), 0);
   std::vector<std::vector<unsigned int>> counters_col(m);
   maxcols = 0;
+          #pragma omp for reduction(max : maxcols)
+
   for (const Triplet &tr : triplets) {
     std::vector<unsigned int> &col_idx = counters_col[tr.row];
-  if (std::find(col_idx.begin(), col_idx.end(), tr.col) == col_idx.end()) {
+    if (std::find(col_idx.begin(), col_idx.end(), tr.col) == col_idx.end()) {
       col_idx.push_back(tr.col);
       if (++counters[tr.row] > maxcols) {
         maxcols = counters[tr.row];
@@ -80,24 +74,26 @@ EllpackMat::EllpackMat(const Triplets &triplets, index_t m, index_t n)
            "You did not reserve enough columns!");
   }
 }
+
 void EllpackMat::mvmult(const Vector &x, Vector &y) const {
-  assert(x.size() == n && "Incompatible vector x size!");
-  assert(y.size() == m && "Incompatible vector y size!");
-  for (index_t i = 0; i < m; ++i) {
-    y.at(i) = 0;
-    for (index_t l = i * maxcols; l < (i + 1) * maxcols; ++l) {
-      if (col[l] == -1) break;
-      y.at(i) += x.at(col[l]) * val[l];
+    #pragma omp parallel for
+    for (index_t i = 0; i < m; ++i) {
+        y[i] = 0;
+        index_t colIndex = i * maxcols;  // Start index for col and val
+        for (index_t l = 0; l < maxcols; ++l, ++colIndex) {
+            if (col[colIndex] == -1) break;
+            y[i] += x[col[colIndex]] * val[colIndex];
+        }
     }
-  }
 }
 
 
-/* SAM_LISTING_BEGIN_5 */
 int main() {
+    omp_set_num_threads(3);
+
   Triplets triplets;
   unsigned int m, n, ntriplets;
-  std::ifstream file("/workspaces/ellpack-spmv/inputs/test_basic_mat_in.txt");
+  std::ifstream file("/workspaces/ellpack-spmv/inputs/Materials Problem/arc130.mtx");
   if (!file) {
     std::cerr << "Error opening file" << std::endl;
     return 1;
@@ -116,17 +112,9 @@ int main() {
 
   // Build Ellpack matrix
   EllpackMat E(triplets, m, n);
-  
- // std::cout << " ------------- Test: print  E ---------------- " << std::endl;
- // for (int i = 0; i < m; ++i) {
- //   for (int j = 0; j < n; ++j) {
-  //    std::cout << std::setw(4) << E(i, j) << " ";
- //   }
- //   std::cout << std::endl;
- // }
-  
+
   std::cout << " ------------- Test of y = E*x --------------- " << std::endl;
-  std::ifstream xFile("/workspaces/ellpack-spmv/inputs/test_basic_vec_in.txt");
+  std::ifstream xFile("/workspaces/ellpack-spmv/inputs/Materials Problem/test_vec_130_random_in.txt");
   if (!xFile) {
     std::cerr << "Error opening x file" << std::endl;
     return 1;
@@ -140,13 +128,16 @@ int main() {
     xFile >> x[i];
   }
 
-  Vector res(m,0);
-  
+  Vector res(m, 0);
+
+  auto start_time = std::chrono::high_resolution_clock::now();
+
+  // Execute the operation you want to test
   E.mvmult(x, res);
-std::cout << "Ellpack E*x =" << std::endl;
-for (const auto& element : res) {
-    std::cout << element << " ";
+
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+
+  // Output the execution time
+  std::cout << "Execution Time: " << duration << " ns" << std::endl;
 }
-std::cout << std::endl;  
-}
-/* SAM_LISTING_END_5 */
